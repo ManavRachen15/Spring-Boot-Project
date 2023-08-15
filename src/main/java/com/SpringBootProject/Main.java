f8a159b8769b250cc3738bb06d34956bea5fffa2
ManavRachen15 <manavrachen@hotmail.com>
2023-08-16 00:43:16 +0100

    Customer class

--- src/main/java/com/SpringBootProject/Main.java
+++ src/main/java/com/SpringBootProject/Main.java
@@ -1,9 +1,14 @@
 package com.SpringBootProject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -12,8 +17,15 @@ public class Main {
         SpringApplication.run(Main.class,args);
     }
 
-    @GetMapping("/")
-    public String greet(){
-        return "hello";
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        GreetResponse response = new GreetResponse(
+                "hello",
+                List.of("Java","Python","JS"),
+                new Person("Manav")
+        );
+        return response;
     }
+    record  Person (String name){}
+    record GreetResponse(String greet, List<String> ProgrammingLanguages, Person person){}
 }
